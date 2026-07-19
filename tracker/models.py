13c239b57e44aa2dd168a4893f51f08dae5d31da
adhithyabiju123-art <adhithya.biju123@gmail.com>
2026-07-19 18:53:37 +0530

    Initial Django project

---
+++ tracker/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class MoodEntry(models.Model):
+    MOOD_CHOICES = [
+        (1, '😥 Awful'),
+        (2, '☹️ Meh'),
+        (3, '😑 okey'),
+        (4, '😊 Good'),
+        (5, '🤩 Amazing'),
+                ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    rating = models.IntegerField(choices=MOOD_CHOICES)
+    note = models.TextField(blank=True,null=True)
+    data_created = models.DateTimeField(auto_now_add=True)
+      
+    def __str__(self):
+        return f"{self.data_created.strFtime('%Y-%m-%d')}-{self.get_rating_display()}"
+    
